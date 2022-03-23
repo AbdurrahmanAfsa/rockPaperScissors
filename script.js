26f3ecc6ae47134e2cc463fb0d2032366d1d75ce
abdurrahmanafsa <abdulrahmanafsa@gmail.com>
2022-03-23 15:09:52 +0200

    write a function for each button to be compared with the computer choice and a winner function to check the first to reach 5 points

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const playerScorePara = document.querySelector(".playerScore")
+const resultPara = document.querySelector('.result')
+const computerScorePara = document.querySelector('.computerScore');
+const computerChoicePara = document.querySelector('.computerChoice');
+
 function computerPlay(){
     let choice = ["rock" , "paper" , "scissors"];
     let compChoice = choice[Math.floor(Math.random() * choice.length)];
@@ -7,51 +15,110 @@ function computerPlay(){
 let computerScore = 0;
 let playerScore = 0;
 
-function roundPlay(){
-        let playerChoice = prompt("Choose rock, paper or scissors").trim().toLowerCase();
-        let computerChoice = computerPlay();
-        if(playerChoice === "rock" || "paper" || "scissors"){
-            if(playerChoice === computerChoice){
-                playerScore = playerScore;
-                computerScore = computerScore;
-                alert(`It's a tie!! the computer chose: ${computerChoice}
-                       the result: ${playerScore} - ${computerScore}`);
-            }else if(playerChoice === "rock" && computerChoice ==="scissors"){
-                playerScore += 1
-                alert(`You win!! the computer chose: ${computerChoice}
-                       the result: ${playerScore} - ${computerScore}`);
-            }else if(playerChoice === "paper" && computerChoice ==="rock"){
-                playerScore += 1
-                alert(`You win!! the computer chose: ${computerChoice}
-                       the result: ${playerScore} - ${computerScore}`);
-            }else if(playerChoice === "scissors" && computerChoice ==="paper"){
-                playerScore += 1
-                alert(`You win!! the computer chose: ${computerChoice}
-                       the result: ${playerScore} - ${computerScore}`);
-            }else {
-                computerScore += 1
-                alert(`You lose :( the computer chose: ${computerChoice}
-                       the result: ${playerScore} - ${computerScore}`);
-            }
-        }
-        return (playerScore , computerScore);
-}
 
-function checkWinner(){
-    if (playerScore > computerScore){
-        alert("Congratulations, You win");
-    }else if (playerScore < computerScore){
-        alert("The computer won, good luck next time");
-    }else{
-        alert("It's a tie");
+rock.addEventListener("click" , () => {
+    let computerChoice = computerPlay();
+    let playerChoice = "rock";
+    computerChoicePara.innerHTML = `computer choice: ${computerChoice}`;
+    if(playerChoice === computerChoice){
+        playerScore = playerScore;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: It's a tie`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "paper"){
+        playerScore = playerScore;
+        computerScore += 1;
+        resultPara.innerHTML = `result: Computer wins`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "scissors") {
+        playerScore += 1;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: You win!!!`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }
+    winner();
+});
+
+paper.addEventListener("click" , () => {
+    let computerChoice = computerPlay();
+    let playerChoice = "paper";
+    computerChoicePara.innerHTML = `computer choice: ${computerChoice}`;
+    if(playerChoice === computerChoice){
+        playerScore = playerScore;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: It's a tie`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "scissors"){
+        playerScore = playerScore;
+        computerScore += 1;
+        resultPara.innerHTML = `result: Computer wins`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "rock") {
+        playerScore += 1;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: You win!!!`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }
+    winner();
+});
+
+scissors.addEventListener("click" , () => {
+    let computerChoice = computerPlay();
+    let playerChoice = "scissors";
+    computerChoicePara.innerHTML = `computer choice: ${computerChoice}`;
+    if(playerChoice === computerChoice){
+        playerScore = playerScore;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: It's a tie`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "rock"){
+        playerScore = playerScore;
+        computerScore += 1;
+        resultPara.innerHTML = `result: Computer wins`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }else if(computerChoice === "paper") {
+        playerScore += 1;
+        computerScore = computerScore;
+        resultPara.innerHTML = `result: You win!!!`;
+        playerScorePara.innerHTML = `Player Score: ${playerScore} `;
+        computerScorePara.innerHTML = `Computer Score: ${computerScore}`;
+    }
+    winner();
+});
+
+function winner(){
+    if (playerScore === 5){
+        resultPara.innerHTML = `YOU WON !!!
+                        Click here to play again`;
+        resultPara.addEventListener("click", () => {
+            computerChoicePara.innerHTML = `computer choice: `
+            playerScorePara.innerHTML= `player score: `
+            computerScorePara.innerHTML = `computer score: `
+            resultPara.innerHTML = `result: `
+            playerScore = 0;
+            computerScore = 0;
+        }) 
+    }else if (computerScore === 5){
+        resultPara.innerHTML = `The computer won, good luck next time
+                                       Click here to play again`;
+        resultPara.addEventListener("click", () => {
+            computerChoicePara.innerHTML = `computer choice: `
+            playerScorePara.innerHTML= `player score: `
+            computerScorePara.innerHTML = `computer score: `
+            resultPara.innerHTML = `result: `
+            playerScore = 0;
+            computerScore = 0;
+        })  
     }
 }
 
 
-
-for (let i = 0; i <= 4; i++) {
-    roundPlay();  
-}
-
-checkWinner();
 
